f653ad61b136d9ad2b9f9c5e6c32dda15da0d65b
pepe132 <jose.garciadom4@gmail.com>
2022-07-20 10:21:15 -0500

    Add mongoose and estructure design to YelpCamp

--- app.js
+++ app.js
@@ -3,43 +3,95 @@ const express = require('express');
 const app = express()
 const port = 8080
 
+const Campground=require('./models/campground')
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/yelp_camp');
+
+/*Campground.create(
+    {
+        name:'Sale Hill',
+        image:'https://pixabay.com/get/g281ae17f14c48d221816868e935460e4c40f247d23b2b8ff258ba0955484349ae4ff67ab9212f0edfb903d915f3620b6_340.jpg'
+    },
+    function (err,campground){
+        if (err) {
+            console.log(err);
+            
+        }else{
+            console.log('NEWLY created camprogund');
+            console.log(campground);
+
+        }
+        
+    }
+)*/
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine','ejs')
 
-const campgrounds=[
-    {name:'Salmon creek',image:'https://pixabay.com/get/gc36055c734928575a588e73952050fcf60f63d18b37a542837a9413ab2174cff1c12e46a237678a8e230d418e2ea3361_340.jpg'},
-    {name:'Siko creek',image:'https://pixabay.com/get/g281ae17f14c48d221816868e935460e4c40f247d23b2b8ff258ba0955484349ae4ff67ab9212f0edfb903d915f3620b6_340.jpg'},
-    {name:'Mountain creek',image:'https://pixabay.com/get/gd0f496b5feeca632ceb7165226adef6b35c01c77ee9a73ebbe069568c6af0a33284cf8ff8f82deded35892267b4c5190_340.jpg'},
-    {name:'Salmon creek',image:'https://pixabay.com/get/gc36055c734928575a588e73952050fcf60f63d18b37a542837a9413ab2174cff1c12e46a237678a8e230d418e2ea3361_340.jpg'},
-    {name:'Siko creek',image:'https://pixabay.com/get/g281ae17f14c48d221816868e935460e4c40f247d23b2b8ff258ba0955484349ae4ff67ab9212f0edfb903d915f3620b6_340.jpg'},
-    {name:'Mountain creek',image:'https://pixabay.com/get/gd0f496b5feeca632ceb7165226adef6b35c01c77ee9a73ebbe069568c6af0a33284cf8ff8f82deded35892267b4c5190_340.jpg'},
-]
 
 app.get('/',(req,res)=>{
     res.render('landing')
 
 })
 
+//INDEX-show all campgrounds
 app.get('/campgrounds',(req,res)=>{
+    //get all campgrounds frmo db
+
+    Campground.find({},(err,allCampgrounds)=>{
+        if (err) {
+            console.log(err);
+            
+        }else{
+
+            res.render('index',{campgrounds:allCampgrounds})
+        }
+
+    })
     
-    res.render('campgrounds',{campgrounds:campgrounds})
 
 })
 
+//NEW-show form to create new campground
 app.get('/campgrounds/new',(req,res)=>{
     res.render("new")
 
 })
 
+//SHOW-shows more info in campgrounds
+app.get('/campgrounds/:id',(req,res)=>{
+    //find the campground with provided ID
+    Campground.findById(req.params.id,(err,foundCamp)=>{
+        if (err) {
+            console.log(err);
+            
+        }else{
+            //render show template with that campground
+            res.render('show',{campground:foundCamp})
+
+        }
+    })
+})
+
+//CREATE-add new campground to DB
 app.post('/campgrounds',(req,res)=>{
     const name=req.body.name
     const image=req.body.image
-    const newCamp={name:name,image:image}
+    const desc=req.body.description
+    const newCamp={name:name,image:image,description:desc}
 
-    campgrounds.push(newCamp)
+    //create a new campground and save to db
 
-    res.redirect("/campgrounds")
+    Campground.create(newCamp,(err,newly)=>{
+        if (err){
+            console.log(err);
+            
+        }else{
 
+            res.redirect("/campgrounds")
+        }
+    })
 })
 
 app.listen(port,function() {
